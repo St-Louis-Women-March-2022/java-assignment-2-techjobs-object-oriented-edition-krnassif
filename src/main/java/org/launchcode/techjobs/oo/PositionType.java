f9cb89ed8ce3462b6ce6a76d3249733c5d09d592
Katie <krnassif@yahoo.com>
2022-09-26 23:08:48 -0500

    First test for ToString

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -18,31 +18,23 @@ public class PositionType {
         this.value = value;
     }
 
-    // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
-
-    // Getters and Setters:
-
-//       @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        PositionType that = (PositionType) o;
-//        return id == that.id && Objects.equals(value, that.value);
-//    }
-
     @Override
     public int hashCode() {
 
         return Objects.hash(id, value);
     }
 
+    // TODO: Add a custom toString() method that returns the data stored in 'value'.
+
     public String toString() {
 
         return value;
     }
+
+    // Getters and Setters:
 
     public int getId() {
 
